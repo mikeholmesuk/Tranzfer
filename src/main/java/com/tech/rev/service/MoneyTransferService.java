c937f5337ced8efd290b01adb4ee5f9b7e833f0e
Mike <mike@myhometouch.com>
2017-09-11 21:56:19 +0100

    Amended the `handleMoneyTransfer` method to be synchroniz(s)ed to ensure thread-safety. Amended the return type to return the persisted transfer record details. Added additional check to validate the number of updates on the account records.

--- src/main/java/com/tech/rev/service/MoneyTransferService.java
+++ src/main/java/com/tech/rev/service/MoneyTransferService.java
@@ -5,10 +5,7 @@ import com.tech.rev.dao.TransferDAO;
 import com.tech.rev.exception.InsufficientFundsException;
 import com.tech.rev.model.Account;
 import com.tech.rev.model.Transfer;
-import org.skife.jdbi.v2.DBI;
-import org.skife.jdbi.v2.Handle;
-import org.skife.jdbi.v2.TransactionCallback;
-import org.skife.jdbi.v2.TransactionStatus;
+import org.skife.jdbi.v2.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,9 +14,9 @@ import java.math.BigDecimal;
 public class MoneyTransferService {
     Logger logger = LoggerFactory.getLogger(MoneyTransferService.class);
 
-    private DBI database;
-    private AccountService accountService;
-    private TransferDAO transferDAO;
+    private final DBI database;
+    private final AccountService accountService;
+    private final TransferDAO transferDAO;
 
     public MoneyTransferService(DBI database, AccountService accountService, TransferDAO transferDAO) {
         this.database = database;
@@ -27,7 +24,7 @@ public class MoneyTransferService {
         this.transferDAO = transferDAO;
     }
 
-    public Transfer handleMoneyTransfer(final Transfer transfer) {
+    public synchronized Transfer handleMoneyTransfer(final Transfer transfer) {
         // Grab the accounts (I might refactor this out to a service)
         final Account debitAccount = accountService.fetchByAccountNumber(transfer.getDebitAccountNo());
         final Account creditAccount = accountService.fetchByAccountNumber(transfer.getCreditAccountNo());
@@ -36,12 +33,12 @@ public class MoneyTransferService {
         if (debitAccount.getAccountBalance().subtract(transfer.getAmount()).compareTo(BigDecimal.ZERO) < 0) {
             throw new InsufficientFundsException(String.format("Account # %s does not have  enough funds " +
                     "to make a £%s transfer", debitAccount.getAccountNumber(), transfer.getAmount()));
-        };
+        }
 
-        return database.inTransaction(
-            new TransactionCallback<Transfer>() {
+        Integer transferId = database.inTransaction(TransactionIsolationLevel.SERIALIZABLE,
+            new TransactionCallback<Integer>() {
 
-                public Transfer inTransaction(Handle handle, TransactionStatus transactionStatus) throws Exception {
+                public Integer inTransaction(Handle handle, TransactionStatus transactionStatus) throws Exception {
                     AccountDAO accountDAOtx = handle.attach(AccountDAO.class);
                     TransferDAO transferDAOtx = handle.attach(TransferDAO.class);
 
@@ -52,14 +49,16 @@ public class MoneyTransferService {
                     Integer creditUpdated = accountDAOtx.updateAccountBalanceById(
                             creditAccount.getId(),
                             creditAccount.getAccountBalance().add(transfer.getAmount()));
-                    Integer transferId = transferDAOtx.createTransfer(transfer);
 
-                    logger.info("Created transfer: " + transferId);
-                    logger.info("Got transfer back:: " + transferDAO.fetchTransferById(transferId));
+                    if (debitUpdated + creditUpdated != 2) {
+                        transactionStatus.setRollbackOnly();
+                    }
 
-                    return transfer;
+                    return  transferDAOtx.createTransfer(transfer);
                 }
             }
         );
+
+        return transferDAO.fetchTransferById(transferId);
     }
 }
